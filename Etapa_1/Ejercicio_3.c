2afe732786d815037bb52be3084bd6e062261332
Diego <dileo312@hotmail.comclear>
2024-01-30 23:34:37 -0600

    Programacion ejercicios

---
+++ Etapa_1/Ejercicio_3.c
@@ -0,0 +1,34 @@
+
+/* 
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+        PROGRAMA QUE CALCULE EL NUEVO SUELDO DEL TRABAJADOR
+        SI GANA $10,000 O MENOS, EL AUMENTO ES DE 15%
+        SI GANA MAS DE $10,000 EL AUMENTO ES DE 8%
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+Autor: Diego Leonardo Alejo Cantu
+Fecha de actualizacion: 26/01/2024
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    int sueldo, sueldo_nuevo;
+
+    printf("Ingresa el sueldo del trabajador: ");
+    scanf("%d", &sueldo);
+
+    if (sueldo <= 10000)
+    {
+        sueldo_nuevo = sueldo * 1.15;
+    }
+    else
+    {
+        sueldo_nuevo = sueldo * 1.08;
+    }
+
+    printf("\nEl sueldo anterior del trabajador era de: $%d\n", sueldo);
+    printf("El trabajador recibio un aumento de $%d\n", sueldo_nuevo - sueldo);
+    printf("El sueldo nuevo del trabajador es de: $%d\n", sueldo_nuevo);
+}
---
+++ Etapa_1/Ejercicio_4.c
@@ -0,0 +1,40 @@
+/* 
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+        PROGRAMA QUE CALCULE EL PRECIO TOTAL DE UN ARTICULO DADO 
+                    SU COSTO
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+Autor: Diego Leonardo Alejo Cantu
+Fecha de actualizacion: 26/01/2024
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    int costo_articulo, precio_impuestos, precio_total;
+
+    printf("Ingresa el costo del articulo: ");
+    scanf("%d", &costo_articulo);
+
+    if (costo_articulo <= 200)
+    
+        precio_impuestos = 0;
+    
+    else if (costo_articulo > 200 && costo_articulo <= 400)
+        
+            precio_impuestos = costo_articulo * 0.30;
+        
+        else if (costo_articulo > 400 && costo_articulo <= 5000)
+            
+                precio_impuestos = costo_articulo * 0.40;
+            
+            else
+            
+                precio_impuestos = costo_articulo * 0.50;
+            
+    precio_total = costo_articulo + precio_impuestos;
+    
+    printf("\nEl precio total final del articulo es de: $%d\n", precio_total);
+    
+}
