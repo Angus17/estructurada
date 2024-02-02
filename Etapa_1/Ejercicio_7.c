050a5b3114996a8023d4f441116fe8f9de23994b
Angus17 <diego.alejocnt@uanl.edu.mx>
2024-02-01 22:02:03 -0600

    Cambios nuevos

--- Etapa_1/Ejercicio_7.c
+++ Etapa_1/Ejercicio_7.c
@@ -9,21 +9,21 @@ int main(void)
     scanf("%d", &entero_y);
 
     if (entero_y <= 11)
-    {
-        entero_y += 50;
-    }
-    else if (entero_y <= 33)
-        {
-            entero_y *= entero_y -10;
-        }
-        else if (entero_y <= 64)
-            {
-                entero_y = pow(entero_y,3) + (entero_y * entero_y) - 10;
-            }
-            else
-            {
-                entero_y = 0;
-            }
-            
     
+        entero_y += 50;
+    
+    else if (entero_y <= 33)
+        
+            entero_y *= entero_y -10;
+        
+        else if (entero_y <= 64)
+            
+                entero_y = pow(entero_y,3) + (entero_y * entero_y) - 10;
+            
+            else
+            
+                entero_y = 0;
+            
+            
+    printf("\nEl nuevo numero es: %d\n", entero_y);
 }
