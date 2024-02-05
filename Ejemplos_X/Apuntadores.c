ae555e0ac13f6140aa134ea1459bebb3105137b8
Diego <dileo312@hotmail.comclear>
2024-02-04 18:47:13 -0600

    Nuevos cambios

--- Ejemplos_X/Apuntadores.c
+++ Ejemplos_X/Apuntadores.c
@@ -4,14 +4,14 @@
 
 int main(void)
 {
-    int numero_1, numero_2, *ap_num, *ap_num_2, suma, resta, multiplicacion, potencia;
-    float division;
+    long long int numero_1, numero_2, *ap_num, *ap_num_2, suma, resta, multiplicacion, potencia;
+    double division;
 
     printf("Ingresa el primer numero: ");
-    scanf("%d", &numero_1);
+    scanf("%lld", &numero_1);
 
     printf("Ingresa el segundo numero: ");
-    scanf("%d", &numero_2);
+    scanf("%lld", &numero_2);
 
     ap_num = &numero_1;
     ap_num_2 = &numero_2;
@@ -19,12 +19,16 @@ int main(void)
     suma = *ap_num + *ap_num_2;
     resta = *ap_num - *ap_num_2;
     multiplicacion = *ap_num * *ap_num_2;
-    division = *ap_num / *ap_num_2;
+    division = (double)(*ap_num) / (*ap_num_2);
     potencia = pow(*ap_num, *ap_num_2);
 
-    printf("\nLa suma es de: %d\n", suma);
-    printf("La resta es de: %d\n", resta);
-    printf("La multiplicacion es de: %d\n", multiplicacion);
-    printf("La division es de: %.2f\n", division);
-    printf("La potencia es de: %d\n", potencia);
+    printf("\nLa suma es de: %lld\n", suma);
+    printf("La resta es de: %lld\n", resta);
+    printf("La multiplicacion es de: %lld\n", multiplicacion);
+    printf("La division es de: %.5f\n", division);
+    printf("La potencia es de: %lld\n", potencia);
+    /* printf("%lld\t%lld", sizeof(float), sizeof(double)); */
+
+    system("pause");
+    system("cls");
 }
