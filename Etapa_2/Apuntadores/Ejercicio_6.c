b1b51cd769ee914434f6c855456f2af7c02b7b66
Angus17 <diego.alejocnt@uanl.edu.mx>
2024-03-02 23:21:49 -0600

    Ejercicio Apuntadores

---
+++ Etapa_2/Apuntadores/Ejercicio_6.c
@@ -0,0 +1,128 @@
+/*  
+    Elabora un programa que mediante una funcion, eleve un numero indicado por el 
+    usuario, a la potencia tambien indicada por el. Validar que los numeros sean
+    mayor a cero.
+
+    Los datos y la impresion se realizan desde main
+
+    La llamada a la funcion es por referencia
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#if __linux__
+    #include <stdio_ext.h>
+#endif 
+
+void elevar_numero(int, int, int *);
+void validar_Sistema_Operativo();
+void limpiar_buffer_STDIN();
+void limpiar_terminal();
+void pausar_terminal();
+
+int main(void)
+{
+    int potencia, valor, numero, resultado;
+
+    do
+    {
+        puts("Escribe un numero positivo: ");
+
+        limpiar_buffer_STDIN();
+
+        valor = scanf("%d", &numero);
+
+        if (numero < 0 || valor != 1)
+        
+            validar_Sistema_Operativo();
+        
+        
+    } while (numero < 0 || valor != 1);
+
+    do
+    {
+        puts("Escribe un numero positivo: ");
+
+        limpiar_buffer_STDIN();
+
+        valor = scanf("%d", &potencia);
+
+        if (potencia < 0 || valor != 1)
+        
+            validar_Sistema_Operativo();
+        
+    } while (potencia < 0 || valor != 1);
+    
+    elevar_numero(numero, potencia, &resultado);
+    
+    printf("El numero %d elevado a la %d es: %d\n", numero, potencia, resultado);
+}
+
+
+void elevar_numero(int NumeroP, int potencia_f, int *resultado_f)
+{
+    int i, resultado = 1;
+
+    for (i = 1; i <= potencia_f; i++)
+    
+        resultado *= NumeroP;
+    
+    *resultado_f = resultado;
+}
+
+// Limpia buffer STDIN tanto para sistemas Windows como para UNIX/Linux
+void limpiar_buffer_STDIN()
+{
+    #if defined(_WIN32) || defined(_WIN64)
+        fflush(stdin);
+    #elif __linux__
+        __fpurge(stdin);
+    #endif
+}
+
+// Limpia terminal tanto para sistemas Windows como para UNIX/Linux
+void limpiar_terminal()
+{
+    #if defined(_WIN32) || defined(_WIN64)
+        system("cls");
+    #elif __linux__
+        system("clear");
+    #endif
+}
+
+// Pausa la terminal tanto para sistemas Windows como para UNIX/Linux
+void pausar_terminal()
+{
+    #if defined(_WIN32) || defined(_WIN64)
+        system("pause");
+    #elif __linux__
+        printf("Presiona ENTER para continuar. . .");
+        fflush(stdout);
+        limpiar_buffer_STDIN();
+        getchar();
+    #endif
+}
+
+// Clasifica los mensajes de error tanto para Windows como para UNIX/Linux, cuando se ingresan datos incorrectos
+void validar_Sistema_Operativo()
+{
+    #if defined(_WIN32) || defined(_WIN64)
+        limpiar_terminal();
+
+        printf("Dato/s ingresado/s no valido/s, verificar dato/s");
+        pausar_terminal();
+        limpiar_terminal();
+    #elif __linux__
+        limpiar_terminal();
+
+        printf("Dato/s ingresado/s no valido/s, verificar dato/s");
+        fflush(stdout);
+        
+        printf("\nPresiona la tecla ENTER para continuar. . . ");
+        fflush(stdout);
+        limpiar_buffer_STDIN();
+        getchar();
+        
+        limpiar_terminal();
+    #endif
+}
